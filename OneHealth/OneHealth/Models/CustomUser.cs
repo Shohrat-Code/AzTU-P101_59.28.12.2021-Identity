73647e6c9c941a6e48d38044c96559b2d8ea29d8
Shohrat-Code <shohrat@code.edu.az>
2021-12-28 14:41:36 +0400

    COdes

---
+++ OneHealth/OneHealth/Models/CustomUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneHealth.Models
+{
+    public class CustomUser:IdentityUser
+    {
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Surname { get; set; }
+        [MaxLength(500)]
+        public string Address { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
